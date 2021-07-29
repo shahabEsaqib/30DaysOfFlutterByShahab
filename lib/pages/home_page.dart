ef8d8ebc7e1b0be6f5862a3adc7f1e0db2373eb0
shahabAta <shahab.sk708@gmail.com>
2021-07-30 04:51:12 +0500

    day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
+//import 'package:demo_project/widgets/drawer.dart';
+import 'package:demo_project/widgets/theme.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:demo_project/models/catalog.dart';
-import 'package:demo_project/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -33,56 +34,163 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    //final dummylist = List.generate(20, (index) => CatalogeModel.items[0]);
     return Scaffold(
-      appBar: AppBar(title: Text('Catalog App')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogeModel.items.isNotEmpty)
-            ? GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemBuilder: (context, index) {
-                  final item = CatalogeModel.items[index];
-                  return Card(
-                      clipBehavior: Clip.antiAlias,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: GridTile(
-                        header: Container(
-                          child: Text(
-                            item.name,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(color: Colors.deepPurple),
-                        ),
-                        child: Image.network(item.image),
-                        footer: Container(
-                          child: Text(
-                            item.price.toString(),
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(color: Colors.deepPurple),
-                        ),
-                      ));
-                },
-                itemCount: CatalogeModel.items.length,
-              )
-            // ListView.builder(
-            //     itemCount: CatalogeModel.items.length,
-            //     itemBuilder: (context, index) =>
-            //         ItemWidget(item: CatalogeModel.items[index]),
-            //   )
-            : Center(
-                child: CircularProgressIndicator(),
-              ),
+      backgroundColor: MyTheme.creemcolor,
+      //  appBar: AppBar(title: Text('Catalog App')),
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatlogHeader(),
+              // ignore: unnecessary_null_comparison
+              if (CatalogeModel.items != null && CatalogeModel.items.isNotEmpty)
+                CatalogList().expand()
+              else
+                Center(
+                  child: CircularProgressIndicator(),
+                )
+            ],
+          ),
+        ),
       ),
-      drawer: MyDrawer(),
+      // body:Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: (CatalogeModel.items.isNotEmpty)
+      //       ? GridView.builder(
+      //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 2,
+      //             crossAxisSpacing: 16,
+      //             mainAxisSpacing: 16,
+      //           ),
+      //           itemBuilder: (context, index) {
+      //             final item = CatalogeModel.items[index];
+      //             return Card(
+      //                 clipBehavior: Clip.antiAlias,
+      //                 shape: RoundedRectangleBorder(
+      //                     borderRadius: BorderRadius.circular(10)),
+      //                 child: GridTile(
+      //                   header: Container(
+      //                     child: Text(
+      //                       item.name,
+      //                       style: TextStyle(color: Colors.white),
+      //                     ),
+      //                     padding: const EdgeInsets.all(12),
+      //                     decoration: BoxDecoration(color: Colors.deepPurple),
+      //                   ),
+      //                   child: Image.network(item.image),
+      //                   footer: Container(
+      //                     child: Text(
+      //                       item.price.toString(),
+      //                       style: TextStyle(color: Colors.white),
+      //                     ),
+      //                     padding: const EdgeInsets.all(12),
+      //                     decoration: BoxDecoration(color: Colors.deepPurple),
+      //                   ),
+      //                 ));
+      //           },
+      //           itemCount: CatalogeModel.items.length,
+      //         )
+      //       // ListView.builder(
+      //       //     itemCount: CatalogeModel.items.length,
+      //       //     itemBuilder: (context, index) =>
+      //       //         ItemWidget(item: CatalogeModel.items[index]),
+      //       //   )
+      //       : Center(
+      //           child: CircularProgressIndicator(),
+      //         ),
+      // ),
+      // drawer: MyDrawer(),
     );
+  }
+}
+
+class CatlogHeader extends StatelessWidget {
+  const CatlogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.bold.xl5.color(MyTheme.darkBluishColor).make(),
+        "trending products".text.xl2.make(),
+      ],
+    );
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogeModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogeModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(
+          image: catalog.image,
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catalog.desc.text.textStyle(context.captionStyle!).make(),
+            12.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              children: [
+                "\$${catalog.price}".text.bold.xl.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(StadiumBorder())),
+                    child: "Buy".text.make())
+              ],
+            ).pOnly(right: 10)
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().py12();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .rounded
+        .p8
+        .color(MyTheme.creemcolor)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
